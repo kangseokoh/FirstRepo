31e943ff75a16d77ceb73987d7b04979523e0cdc
rkdtjr167 <rkdtjr167@naver.com>
2023-03-15 22:05:44 +0900

    SpringStudy2

--- hello-spring/src/main/java/hello/hellospring/Controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/Controller/HelloController.java
@@ -3,16 +3,27 @@ package hello.hellospring.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
+    // GetMapping은 url의 주소에 입력한 값을 찾아감 main/hello면 hello 찾아감
+    // return => templates의 hello.html을 실행시켜라
+    // templates에 없으면 화면 못찾는 에러발생
+    // RequestParam 파라미터는 Mapping값/?변수명=값 으로받음
+
     @GetMapping("hello")
-    // => GetMapping은 url의 주소에 입력한 값을 찾아감 main/hello면 hello 찾아감
     public String hello(Model model) {
         model.addAttribute("data","hello!!");
         return "hello";
-        // return => templates의 hello.html을 실행시켜라
-        // templates에 없으면 화면 못찾는 에러발생
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+
 }
