fdb4f5ad5b11e2883eabaf3e9ea7ce0a8ee5ebf6
rkdtjr167 <rkdtjr167@naver.com>
2023-03-24 23:06:25 +0900

    DI Training

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,2 +1,22 @@
-package hello.core;public class AppConfig {
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.orderService;
+import hello.core.order.orderServiceImpl;
+
+public class AppConfig {
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public orderService orderService() {
+        return new orderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
 }
--- core/src/main/java/hello/core/orderApp.java
+++ core/src/main/java/hello/core/orderApp.java
@@ -1,31 +1,24 @@
 package hello.core;
 
-import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.*;
 import hello.core.order.Order;
 import hello.core.order.orderService;
-import hello.core.order.orderServiceImpl;
 
 public class orderApp {
 
     public static void main(String[] args) {
 
-        MemberService memberService = new MemberServiceImpl();
-        orderService orderService = new orderServiceImpl();
-        MemberRepository memberRepository = new MemoryMemberRepository();
-        DiscountPolicy discountPolicy = new FixDiscountPolicy();
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService = appConfig.memberService();
+        orderService orderService = appConfig.orderService();
 
         Long MemberId = 1L;
         Member member = new Member(MemberId,"memberA", Grade.VIP);
 
         memberService.join(member);
-
         Order order = orderService.createOrder(MemberId,"Item",10000);
-        //Member member1 = memberRepository.FindById(MemberId);
-        int discountPrice = order.calculatePrice();
 
-        System.out.println(discountPrice);
-
+        System.out.println("order : " + order);
     }
 }
