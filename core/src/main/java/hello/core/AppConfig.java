e4f70fa4be84daf64af39e910f8447f7f1028c7e
rkdtjr167 <rkdtjr167@naver.com>
2023-03-25 16:27:02 +0900

    SpringBean Training

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -3,20 +3,37 @@ package hello.core;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.orderService;
 import hello.core.order.orderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
-    public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
     public orderService orderService() {
-        return new orderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new orderServiceImpl(memberRepository(), discountPolicy());
     }
 
 }
--- core/src/main/java/hello/core/orderApp.java
+++ core/src/main/java/hello/core/orderApp.java
@@ -3,21 +3,26 @@ package hello.core;
 import hello.core.member.*;
 import hello.core.order.Order;
 import hello.core.order.orderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class orderApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        orderService orderService = appConfig.orderService();
 
-        MemberService memberService = appConfig.memberService();
-        orderService orderService = appConfig.orderService();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        orderService orderService = ac.getBean("orderService", orderService.class);
 
         Long MemberId = 1L;
         Member member = new Member(MemberId,"memberA", Grade.VIP);
 
         memberService.join(member);
-        Order order = orderService.createOrder(MemberId,"Item",10000);
+        Order order = orderService.createOrder(MemberId,"Item",20000);
 
         System.out.println("order : " + order);
     }
